29d5496c7ca809d3695fe5c1d200876eb3f3d916
Jade McGough <jade@thezets.com>
2018-08-28 15:52:34 -0700

    separate responsibilities within nats publisher

--- nats/publisher.go
+++ nats/publisher.go
@@ -1,6 +1,7 @@
 package nats
 
 import (
+	"errors"
 	"io"
 	"io/ioutil"
 
@@ -8,26 +9,40 @@ import (
 	"go.uber.org/zap"
 )
 
+var ErrNoNatsConnection = errors.New("Nats connection has not been established. Call Open() first.")
+
 type Publisher interface {
+	// Open creates a connection to the nats server
+	Open() error
 	// Similar to Publish in nats
 	Publish(subject string, r io.Reader) error
 }
 
 type publisher struct {
+	ClientID   string
 	Connection stan.Conn
 	Logger     *zap.Logger
 }
 
-func NewPublisher(clientID string) (Publisher, error) {
-	sc, err := stan.Connect(ServerName, clientID)
-	if err != nil {
-		return nil, err
-	}
+func NewPublisher(clientID string) *publisher {
+	p := publisher{ClientID: clientID}
+	return &p
+}
 
-	return &publisher{Connection: sc}, nil
+func (p *publisher) Open() error {
+	sc, err := stan.Connect(ServerName, p.ClientID)
+	if err != nil {
+		return err
+	}
+	p.Connection = sc
+	return nil
 }
 
 func (p *publisher) Publish(subject string, r io.Reader) error {
+	if p.Connection == nil {
+		return ErrNoNatsConnection
+	}
+
 	ah := func(guid string, err error) {
 		if err != nil {
 			p.Logger.Info(err.Error())
